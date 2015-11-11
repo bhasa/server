a47d7c2b6c9fb7eeafa61e7a2125a3cbb3002db8
Alex Gordon <alextgordon@gmail.com>
2015-11-11 00:32:27 +0000

    hopefully this will work

--- src/main.js
+++ src/main.js
@@ -50,7 +50,13 @@ if (DEBUG) {
     })
 }
 else {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    // port:     match[4],
+    // host:     match[3],
+    logging: true //false
+  });
 }
 
 var Version = sequelize.define('Version', {
