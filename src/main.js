493fc4a44da87894fe1027ef43107f0852179659
Alex Gordon <alextgordon@gmail.com>
2015-11-11 00:37:35 +0000

    ???

--- src/main.js
+++ src/main.js
@@ -53,9 +53,6 @@ else {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    // port:     match[4],
-    // host:     match[3],
-    logging: true //false
   });
 }
 
